9d0085f12061892c6e9bfbe8a17438b06de80c54
Danillo Neves Souza <danillo.neves97@gmail.com>
2020-03-03 14:48:59 -0300

    Stream Creation Submission Implemented
    The app now shows a form (with value check) in /streams/new
    
    Will implement the RESTful API server next to actually handle submissions
    
    Extra: Installed Redux Form and added some code for debugging sessions and Redux DevTools

--- streams/client/src/components/streams/StreamCreate.js
+++ streams/client/src/components/streams/StreamCreate.js
@@ -1,11 +1,69 @@
 import React from 'react';
+import { Field, reduxForm } from 'redux-form';
+import { Form, Input, Button, Checkbox } from 'antd';
 
-const StreamCreate = () => {
-    return (
-        <div>
-            StreamCreate
-        </div>
-    )
+const layout = {
+    labelCol: { span: 8 },
+    wrapperCol: { span: 16 },
+};
+  
+const tailLayout = {
+    wrapperCol: { offset: 8, span: 16 },
+};
+
+class StreamCreate extends React.Component {
+    
+    renderInput({input}) {
+        return (
+            <input { ...input } />
+        )
+    }
+
+    onFinish (formValues) {
+        console.log(formValues)
+    }
+    
+    render (){
+        return (
+            <div>
+                <Form onFinish={this.props.handleSubmit(this.onFinish)}
+                {...layout}
+                name="basic"
+                initialValues={{ remember: true }}
+                >
+                    <Form.Item
+                    label="Title"
+                    name="title"
+                    rules={[{ required: true, message: 'Please input a title!'}]}
+                    >
+                        <Field name="title" component={this.renderInput}/>
+                    </Form.Item>
+
+                    <Form.Item
+                    label="Description"
+                    name="description"
+                    rules={[{ required: true, message: 'Please input a description!' }]}
+                    >
+                        <Field name="description" component={this.renderInput}/>
+                    </Form.Item>
+
+                    <Form.Item {...tailLayout} name="remember" valuePropName="checked">
+                        <Checkbox>Remember me</Checkbox>
+                    </Form.Item>
+
+                    <Form.Item {...tailLayout}>
+                    <Button type="primary" htmlType="submit">
+                        Submit
+                    </Button>
+                    </Form.Item>
+                </Form>
+            </div>
+        )
+    }
 }
 
-export default StreamCreate;
+
+
+export default reduxForm({
+    form: 'streamCreate'
+})(StreamCreate);
